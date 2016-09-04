13099000863ab692ba3cadf55ddaa8f59ec45b7f
Steve Bargelt <steve@bargelt.com>
2016-09-04 10:09:24 -0700

    Plugin install during image build would be fantastic

--- jenkins-master/plugins.sh
+++ jenkins-master/plugins.sh
@@ -20,10 +20,13 @@ then
 USAGE:
   Parse a support-core plugin -style txt file as specification for jenkins plugins to be installed
   in the reference directory, so user can define a derived Docker image with just :
+
   FROM jenkins
   COPY plugins.txt /plugins.txt
   RUN /usr/local/bin/plugins.sh /plugins.txt
+
   Note: Plugins already installed are skipped
+
 "
     exit 1
 else
